d853bb06849808173c5d30f3d35cff3afd8094bd
Nouman Mehmood <nouman_7@live.co.uk>
2021-05-03 13:29:44 +0100

    Initial Commit

---
+++ guardian-newsfeed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guardian-newsfeed
+//
+//  Created by Nouman on 03/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ guardian-newsfeed/guardian_newsfeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  guardian_newsfeedApp.swift
+//  guardian-newsfeed
+//
+//  Created by Nouman on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct guardian_newsfeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
